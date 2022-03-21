745e3b0b7231aa2ae539102161a9bed1870429bc
WahyuS002 <syahputrawahyu61@gmail.com>
2022-03-21 21:55:11 +0700

    :hammer: quick fix href in anchor tag

--- src/App.js
+++ src/App.js
@@ -154,9 +154,9 @@ function App() {
         <>
             <ToastContainer />
             <div className="min-h-screen font-caveat-brush selection:bg-primary selection:text-gray-800">
-                <div className="flex justify-center md:block">
+                <a href="https://metaheroesclub.com/" target={'_blank'} className="flex justify-center md:block" rel="noreferrer">
                     <img className="absolute w-72 p-5" draggable={false} src={logoYellow} alt="Logo Yellow" />
-                </div>
+                </a>
                 <div className="hidden md:block">
                     <div className="absolute max-w-full inset-0 -z-20 min-h-screen bg-no-repeat bg-[url('./assets/background.png')] bg-cover"></div>
                 </div>
@@ -268,7 +268,7 @@ function App() {
                                 <path d="M5.026 15c6.038 0 9.341-5.003 9.341-9.334 0-.14 0-.282-.006-.422A6.685 6.685 0 0 0 16 3.542a6.658 6.658 0 0 1-1.889.518 3.301 3.301 0 0 0 1.447-1.817 6.533 6.533 0 0 1-2.087.793A3.286 3.286 0 0 0 7.875 6.03a9.325 9.325 0 0 1-6.767-3.429 3.289 3.289 0 0 0 1.018 4.382A3.323 3.323 0 0 1 .64 6.575v.045a3.288 3.288 0 0 0 2.632 3.218 3.203 3.203 0 0 1-.865.115 3.23 3.23 0 0 1-.614-.057 3.283 3.283 0 0 0 3.067 2.277A6.588 6.588 0 0 1 .78 13.58a6.32 6.32 0 0 1-.78-.045A9.344 9.344 0 0 0 5.026 15z"></path>
                             </svg>
                         </a>
-                        <a href="discord.gg/Z9bZ3VZRZQ">
+                        <a href="https://discord.gg/Z9bZ3VZRZQ" target={'_blank'} rel="noreferrer">
                             <svg
                                 xmlns="http://www.w3.org/2000/svg"
                                 fill="currentColor"
